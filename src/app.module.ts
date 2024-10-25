430552ccd6bd7bc986f18d8c86521ba1292cbf5e
lsh4711 <dowkwll@naver.com>
2024-10-25 18:58:11 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { AuthModule } from '@/auth/auth.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberModule } from './member/member.module';
 
 @Module({
-  imports: [MemberModule],
+  imports: [MemberModule, MikroOrmModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
