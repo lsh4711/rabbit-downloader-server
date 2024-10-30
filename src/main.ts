5093530a2f8eee62a930ab2c47071ea425c941a6
lsh4711 <dowkwll@naver.com>
2024-10-30 22:00:05 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { SwaggerUtil } from '@/utils/swagger.util';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  SwaggerUtil.runtimeSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
