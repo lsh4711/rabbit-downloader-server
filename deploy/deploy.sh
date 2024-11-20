f28d39c59b93c0359c4091e90e191a660dcaed25
lsh4711 <dowkwll@naver.com>
2024-11-20 11:09:33 +0900

    배포 환경 설정 변경
    - docker compose 서비스 실행 여부 체크 로직 변경
    - 배포 스크립트 결과 출력 추가
    - nginx port 지정 및 ipv6 listen 추가
    - server 이미지에 curl 추가

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,24 +4,28 @@ load_env() {
 
 is_running() {
   local service=${1}
-  docker-compose ps -q "${service}" > /dev/null 2>&1
+  docker compose ps "${service}" | grep Up > /dev/null
   return ${?}
 }
 
 deploy() {
   local on=${1}
   local off=${2}
-  docker compose up -d --wait "${on}"
+  docker compose up "${on}" --detach --wait
   reload_nginx "${on}"
   sleep 3s
   docker compose down "${off}"
+  echo
+  echo "${on}: on"
+  echo "${off}: off"
 }
 
 reload_nginx() {
   local host=${1}
   docker compose up nginx --detach --wait --no-recreate
-  SERVER_HOST=${host} \
-  docker compose exec nginx ./docker-entrypoint.d/20-envsubst-on-templates.sh && nginx -s reload
+  docker compose exec nginx sh -c \
+  "SERVER_HOST=${host} \
+  ./docker-entrypoint.d/20-envsubst-on-templates.sh && nginx -s reload"
 }
 
 run() {
